9134bf19807bc466dcee05c8f6e9c9f58c733325
tjapa <nicotamalu@gmail.com>
2024-04-19 17:26:37 -0300

    add api gateway and lambda function

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,43 @@
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import * as cwlogs from 'aws-cdk-lib/aws-logs'
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+    super(scope, id, props)
+
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs')
+
+    const api = new apigateway.RestApi(this, 'ECommerceApi', {
+      restApiName: 'ECommerceApi',
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
+    })
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler,
+    )
+
+    const productsResource = api.root.addResource('products')
+    productsResource.addMethod('GET', productsFetchIntegration)
+  }
+}
